c4215b7c9da396a80d5a2d2f9c8cbf365dfcb5f4
thehappycode <thehappycode@hotmail.com>
2026-06-07 18:44:08 +0700

    chapter3: Initial Project

---
+++ create-projects.bash
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+mkdir microservices
+cd microservices
+
+spring init \
+--boot-version=4.0.6 \
+--type=gradle-project \
+--java-version=21 \
+--packaging=jar \
+--name=product-service \
+--package-name=com.thehappycode.microservices.core.product \
+--groupId=com.thehappycode.microservices.core.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-service
+
+spring init \
+--boot-version=4.0.6 \
+--type=gradle-project \
+--java-version=21 \
+--packaging=jar \
+--name=review-service \
+--package-name=com.thehappycode.microservices.core.review \
+--groupId=com.thehappycode.microservices.core.review \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+review-service
+
+spring init \
+--boot-version=4.0.6 \
+--type=gradle-project \
+--java-version=21 \
+--packaging=jar \
+--name=recommendation-service \
+--package-name=com.thehappycode.microservices.core.recommendation \
+--groupId=com.thehappycode.microservices.core.recommendation \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+recommendation-service
+
+spring init \
+--boot-version=4.0.6 \
+--type=gradle-project \
+--java-version=21 \
+--packaging=jar \
+--name=product-composite-service \
+--package-name=com.thehappycode.microservices.composite.product \
+--groupId=com.thehappycode.microservices.composite.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-composite-service
+
+cd ..
