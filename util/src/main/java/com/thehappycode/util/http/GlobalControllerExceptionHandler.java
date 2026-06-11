f5d8cbd85e2ae3fb8127a4783c304b6a8ea265a8
thehappycode <thehappycode@hotmail.com>
2026-06-12 00:30:26 +0700

    chapter3: Cập nhật exception

---
+++ util/src/main/java/com/thehappycode/util/http/GlobalControllerExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.thehappycode.util.http;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_CONTENT;
+
+import com.thehappycode.api.exceptions.InvalidInputException;
+import com.thehappycode.api.exceptions.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public @ResponseBody HttpErrorInfo handlerNotFoundExceptions(
+            ServerHttpRequest request, NotFoundException ex) {
+        return createHttpErrorInfo(NOT_FOUND, request, ex);
+    }
+
+    @ResponseStatus(UNPROCESSABLE_CONTENT)
+    @ExceptionHandler(InvalidInputException.class)
+    public @ResponseBody HttpErrorInfo handleInvalidInputException(
+            ServerHttpRequest request, InvalidInputException ex) {
+        return createHttpErrorInfo(UNPROCESSABLE_CONTENT, request, ex);
+    }
+
+    private HttpErrorInfo createHttpErrorInfo(
+            HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+
+        final String path = request.getPath().pathWithinApplication().value();
+        final String message = ex.getMessage();
+
+        LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
+        return new HttpErrorInfo(httpStatus, path, message);
+    }
+}
