69449fd6946fd2f97f3ab0dd3805552c25b6a203
thehappycode <thehappycode@hotmail.com>
2026-06-13 14:09:05 +0700

    chapter3: Cập nhật api/product/{productId}

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # microservices-with-spring-boot
+
+## CLI
+
+### Build
+
+```gradlew
+$./gradlew build -x test;
+$./gradlew :microservices:product-service:build -x test;
+```
+
+### Run
+
+> java -jar microservices/product-service/build/libs/\*.jar &
+
+### Kill port
+
+> kill -9 $(lsof -ti:<port>)
---
+++ microservices/product-service/src/main/java/com/thehappycode/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,46 @@
+package com.thehappycode.microservices.core.product.services;
+
+import com.thehappycode.api.core.product.Product;
+import com.thehappycode.api.core.product.ProductService;
+import com.thehappycode.api.exceptions.InvalidInputException;
+import com.thehappycode.api.exceptions.NotFoundException;
+import com.thehappycode.util.http.ServiceUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+    public static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(
+            ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    /**
+     * Lấy Product theo productId
+     * 
+     * @param productId
+     * @return Product
+     */
+    @Override
+    public Product getProduct(@PathVariable int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+        if (productId == 13) {
+            throw new NotFoundException("No product found for productId: " + productId);
+        }
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
