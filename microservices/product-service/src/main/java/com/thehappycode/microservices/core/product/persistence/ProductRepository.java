f82294855bab66646c15e7302a5114fc434314be
thehappycode <thehappycode@hotmail.com>
2026-07-04 23:05:15 +0700

    chapter6: Add Repository

---
+++ microservices/product-service/src/main/java/com/thehappycode/microservices/core/product/persistence/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.thehappycode.microservices.core.product.persistence;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProductRepository extends
+        PagingAndSortingRepository<ProductEntity, String>,
+        CrudRepository<ProductEntity, String> {
+
+    Optional<ProductEntity> findByProductId(int productId);
+}
---
+++ microservices/recommendation-service/src/main/java/com/thehappycode/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -0,0 +1,11 @@
+package com.thehappycode.microservices.core.recommendation.persistence;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecommendationRepository extends
+        CrudRepository<RecommendationEntity, String> {
+
+    List<RecommendationEntity> findByProductId(int productId);
+}
