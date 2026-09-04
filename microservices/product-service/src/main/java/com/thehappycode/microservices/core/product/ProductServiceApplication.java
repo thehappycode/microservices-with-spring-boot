38d9691ba7b200550a3250ba5c2f1d23d6cceda1
thehappycode <thehappycode@hotmail.com>
2026-09-05 00:06:21 +0700

    chapter6: product-service - Cập nhật cấu hình mongodb.

--- microservices/product-service/src/main/java/com/thehappycode/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/com/thehappycode/microservices/core/product/ProductServiceApplication.java
@@ -1,15 +1,23 @@
 package com.thehappycode.microservices.core.product;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.thehappycode")
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceApplication.class);
 
+	public static void main(String[] args) {
+	    ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
+        String mongoDBHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+        String mongoDBPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+        LOG.info("Connected to MongoDB: " + mongoDBHost + ":" + mongoDBPort);
+
+	}
 }
